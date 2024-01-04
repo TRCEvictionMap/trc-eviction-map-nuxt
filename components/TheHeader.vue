f04c30130cd750cdeeaa195c31e28b97207c4287
jacoacoacob <jacoacoacob@protonmail.com>
2024-01-04 09:50:13 -0600

    made TRC header icon link to homepage

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -7,12 +7,14 @@ const disclosures = useDisclosures();
 
 <template>
     <header class="flex justify-between items-center py-1 px-4 text-trc-blue-700 border-b border-trc-blue-700/20">
-        <NuxtPicture
-            format="webp"
-            src="/trc_logo_horizontal_color.png"
-            alt="Tenant Resource Center logo"
-            sizes="130px sm:150px"
-        />
+        <NuxtLink to="https://www.tenantresourcecenter.org/" target="_blank" rel="noopener noreferrer">
+            <NuxtPicture
+                format="webp"
+                src="/trc_logo_horizontal_color.png"
+                alt="Tenant Resource Center logo"
+                sizes="130px sm:150px"
+            />
+        </NuxtLink>
         <div class="flex py-1 sm:py-3">
             <NavDrawer class="sm:hidden" />
             <nav class="hidden sm:block">
