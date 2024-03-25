260ca3b7e3094f6a905f2d6792a7ec0a37125476
jacoacoacob <jacoacoacob@protonmail.com>
2024-03-25 11:53:02 -0500

    fixed overflow issue with center-justified flex items in DetailsDisclosurePanel

--- components/DetailDisclosurePanel.vue
+++ components/DetailDisclosurePanel.vue
@@ -18,13 +18,13 @@ function onClose(close: () => void) {
   <DisclosurePanel
     class="
       h-1/2 top-0 pb-2 border-t border-trc-blue-700/20 bg-slate-100 
-      elative flex flex-col items-center  overflow-auto
+      flex flex-col items-center overflow-auto
     "
     #="{ close }: { close: () => void }"
   >
     <button
       class="
-        rounded shadow-lg mt-4
+        rounded shadow-lg mt-4 sticky left-[calc(50%-64px)] w-32
         bg-trc-blue-500 text-white p-2 font-mono text-sm font-bold
       "
       @click="() => onClose(close)"
@@ -32,8 +32,8 @@ function onClose(close: () => void) {
       <span>Hide details</span>
     </button>
     <div class="
-      flex flex-col justify-center items-center space-y-4 mt-4
-      sm:flex-row sm:space-x-4 sm:space-y-0 
+      flex flex-col justify-center items-center space-y-4 mt-4 m-auto px-4
+      sm:flex-row sm:space-x-4 sm:space-y-0
     ">
       <FeatureDetail
         v-for="featureId in featureState.selectedFeatures"
