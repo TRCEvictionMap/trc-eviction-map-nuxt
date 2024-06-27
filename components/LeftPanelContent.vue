aad9bf4c3081188f142c821955a64cf337095374
jacoacoacob <jacoacoacob@protonmail.com>
2024-06-27 14:01:19 -0500

    added placeholders and a prompt to click and select map features to side panel

--- components/LeftPanelContent.vue
+++ components/LeftPanelContent.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { useFeatureFlags } from "~/stores/feature-flags";
 import { useFeatureState } from "~/stores/feature-state-store";
+import { useMapControlsV2 } from "~/stores/map-controls-store-v2";
 
+const controls = useMapControlsV2();
 const flags = useFeatureFlags();
 const featureState = useFeatureState();
 </script>
 
 <template>
-  <div class="space-y-6 w-full">
+  <div class="space-y-6 w-full h-full">
     <MapControlsV2 position="left" />
     <TransitionGroup name="features">
       <FeatureDetailV2
@@ -15,11 +17,24 @@ const featureState = useFeatureState();
         :key="featureId"
         :featureId="featureId"
       />
+      <div key="placeholders" v-if="featureState.selectedFeatures.length > 0" class="space-y-6">
+        <div
+          v-for="_ in 3 - featureState.selectedFeatures.length"
+          class="bg-slate-100 p-4 rounded border"
+        >
+          <p>Select another {{ controls.currentSourceHumanReadable }}</p>
+        </div>
+      </div>
       <FeatureDetailChart
         v-if="!flags.disableChart && featureState.selectedFeatures.length > 0"
         class="w-full"
       />
     </TransitionGroup>
+    <div class="flex justify-center items-center pt-12">
+      <p v-if="featureState.selectedFeatures.length === 0" class="">
+        Click a {{ controls.currentSourceHumanReadable }} on the map
+      </p>
+    </div>
   </div>
 </template>
 
