4ff1c7c51362b1531ac01de3bf7c02b3ccb0b8dc
jacoacoacob <jacoacoacob@protonmail.com>
2024-07-20 13:10:21 -0400

    Resolves #46. Tooltip text TBD

--- composables/use-map-popup.ts
+++ composables/use-map-popup.ts
@@ -1,11 +1,24 @@
 import mapboxgl from "mapbox-gl";
+import { useFeatureFlags } from "~/stores/feature-flags";
 import { useFeaturePropertiesV2 } from "~/stores/feature-properties-store-v2";
 import { useFeatureState } from "~/stores/feature-state-store";
+import { useMapControlsV2 } from "~/stores/map-controls-store-v2";
 
 function useMapPopup() {
-  const markerHeight = 50;
+
+  const flags = useFeatureFlags();
+
+  if (flags.disableMapPopups) {
+    return new mapboxgl.Popup();
+  }
+
+  const featureProperties = useFeaturePropertiesV2();
+  const featureState = useFeatureState();
+  const controls = useMapControlsV2();
+
+  const markerHeight = 15;
   const markerRadius = 10;
-  const linearOffset = 10;
+  const linearOffset = 0;
   const popupOffsets: Record<string, [number, number]> = {
     "top": [0, 0],
     "top-left": [0, 0],
@@ -24,17 +37,30 @@ function useMapPopup() {
     maxWidth: "300px",
   });
 
-  const featureProperties = useFeaturePropertiesV2();
-  const featureState = useFeatureState();
+  const dateRange = computed(() => {
+    const { currentMonthRangeHumanReadable } = controls;
+    if (currentMonthRangeHumanReadable) {
+      const { start, end } = currentMonthRangeHumanReadable;
+      if (start.y === end.y && start.m === end.m) {
+        return `during ${start.m} ${start.y}`;
+      }
+      return `between ${start.m} ${start.y} and ${end.m} ${end.y}`;
+    }
+    return "";
+  });
 
   watch(
     () => featureState.hoveredFeature,
     (featureId) => {
-      const properties = featureProperties.bgChoropleth[featureId ?? ""];
-      if (properties) {
-        const { tr, bg,  } = properties;
+      if (featureId) {
+        const filings = featureProperties.currentMonthRangeFilingCount[featureId];
+        const properties = featureProperties.bgChoropleth[featureId];
+        const { tr, bg } = properties;
         popup.setHTML(`
-          <h1>Tract ${tr}, Block Group ${bg}</h1>
+          <p>
+            <span class="font-semibold">${filings.currentWindow}</span> eviction${filings.currentWindow === 1 ? "" : "s"}
+            were filed in Block Group <span class="font-semibold">${tr}.${bg}</span> ${dateRange.value}.
+          </p>
         `);
       }
     },
