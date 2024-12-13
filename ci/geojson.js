f0f7f8e5ea96435e88ca2d155686068e10ed8cc6
Philip Wales <me@ptwales.net>
2024-12-13 14:47:47 -0600

    use service key instead of anon key
    anon key times out at 3 seconds

--- ci/geojson.js
+++ ci/geojson.js
@@ -7,7 +7,7 @@ const { createClient } = require("@supabase/supabase-js");
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY,
+  process.env.SUPABASE_SRVC_KEY
 );
 
 function writeGeojson(name, data) {
